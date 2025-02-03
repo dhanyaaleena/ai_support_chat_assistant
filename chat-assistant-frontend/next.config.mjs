f47085319e836f52cf1f8104508fdf164374ab10
Dhanya <dhanyaaleena@gmail.com>
2025-02-02 18:49:36 -0800

    added basepath

--- chat-assistant-frontend/next.config.mjs
+++ chat-assistant-frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    basePath: '/support-chat', // Set the base path
+    trailingSlash: true,
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/support-chat',
+                basePath: false,
+                permanent: false
+            }
+        ]
+    }
+  };
+  
 export default nextConfig;
